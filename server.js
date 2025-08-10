d94f27c29868f88f13aa8f8fb0800658e5dba7bc
thalodsanjeev <sanjeev.thalod@gastos.in>
2025-08-10 11:51:51 +0530

    cors-update

--- server.js
+++ server.js
@@ -1,33 +1,42 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-// import bodyParser from "body-parser";
 import chatRouter from "./routes/chatRoutes.js";
 import authRouter from "./routes/authRoutes.js";
 
 dotenv.config();
-
 const app = express();
 
 // === Middlewares ===
-// Parses incoming JSON requests
 app.use(express.json());
-
-// Parses URL-encoded bodies (as sent by HTML forms)
 app.use(express.urlencoded({ extended: true }));
 
-// Enable Cross-Origin Resource Sharing (CORS)
-app.use(cors());
-// app.use(bodyParser);
+
+// CORS
+const allowedOrigins = [
+  'http://localhost:3000',
+  'http://localhost:5173',
+  'https://agent-mira-frontend.vercel.app',
+];
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, origin);
+    } else {
+      return callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+}));
 
 
-// Health check route
+//Routes
 app.get("/", (req, res) => {
   res.send("Server is running 🚀");
 });
-app.use("/api",chatRouter);
-app.use("/api",authRouter);
-
+app.use("/api", chatRouter);
+app.use("/api", authRouter);
 
 
 export default app;
